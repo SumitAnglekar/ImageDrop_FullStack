6402de811f73fe7fe1818da3c4e7ef2269af7a77
SumitAnglekar <sumitanglekar@gmail.com>
2020-07-30 04:45:42 -0400

    Initial backend code and AWS setup

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from "axios";
+
+const UserProfiles = () => {
+
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfile = () =>{
+    axios.get("http://localhost:8080/api/v1/user-profile")
+    .then(res=> {
+      console.log(res);
+      setUserProfiles(res.data);
+    });
+  };
+
+  useEffect(()=> {
+    fetchUserProfile();
+  },[]);
+
+  return userProfiles.map((userProfile, index)=>{
+    
+    return(
+        <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+        </div>
+    );
+  });
+};
+
+function App(){
+    return (
+      <div className="App">
+        <UserProfiles />
+      </div>
+    );
+  }
+
+export default App;
---
+++ src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,28 @@
+package com.amigoscode.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+          "AKIAIKZ2HMYQ2BTLZOYQ",
+          "0p75ligFb2oB+K7dG9Q5CQHm3y++grSQZhNGoYlP"
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+    }
+
+}
